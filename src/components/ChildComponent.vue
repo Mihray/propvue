528f64135c90f0e0e830376a79e097338d862718
Mihray <2636853047@qq.com>
2022-08-31 11:33:24 +0800

    Create ChildComponent.vue

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="ChildComponent">
+        <h3>{{title}}</h3>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+
+    },
+    props:[
+    'title'
+  ],
+  methods:{
+
+  }
+}
+</script>
+<style scoped>
+
+</style>
