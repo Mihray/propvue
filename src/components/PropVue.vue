9d607a372ac850d3ff832a9f6094301988882fdb
Mihray <2636853047@qq.com>
2022-08-31 11:33:29 +0800

    Create PropVue.vue

---
+++ src/components/PropVue.vue
@@ -0,0 +1,15 @@
+<template>
+    <ChildComponent title='你好'/>{{title}}
+</template>
+<script>
+    import { title } from 'process';
+import ChildComponent from './components/ChildComponent.vue'
+export default {
+    components: {
+        ChildComponent
+  },
+}
+</script>
+<style scoped>
+
+</style>
