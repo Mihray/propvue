f2ff1df78a68124b244ab137f815cbb0ef70e2e4
Mihray <2636853047@qq.com>
2022-09-01 18:06:28 +0800

    Update PropVue.vue

--- src/components/PropVue.vue
+++ src/components/PropVue.vue
@@ -1,15 +1,37 @@
 <template>
-    <ChildComponent title='你好'/>{{title}}
-</template>
+    <div>hello</div>
+    <div :style="{fontSize:postsFontSize+'em'}">
+        <ChildComponent 
+        title1="你好" box="box" 
+        v-for="post in posts" 
+        v-bind:key="post.id"
+        v-bind:post="post"
+        v-on:enlarge-text="postsFontSize+=0.1"/></div>
+  </template>
+
 <script>
-    import { title } from 'process';
-import ChildComponent from './components/ChildComponent.vue'
+import ChildComponent from './ChildComponent.vue';
+
+// import ChildComponent from '../src/components/ChildComponent.vue'
+
 export default {
-    components: {
-        ChildComponent
-  },
+    //     components: {
+    //         ChildComponent,
+    //   },
+    data() {
+        return {
+            posts:[
+                {id:1,title:'heihei',},
+                {id:2,title:'xixi'},
+                {id:3,title:'kuku'}
+            ],
+            postsFontSize:1,
+        }
+    },
+    components: { ChildComponent }
 }
 </script>
+
 <style scoped>
 
 </style>
